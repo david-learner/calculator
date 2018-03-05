4ae934bf6a19d5ac57027b387b02141a7be17b71
David <xodnjs2549@naver.com>
2018-03-05 21:15:54 +0900

    문자열 사칙연산 계산기

--- src/com/hard/learner/Calculator.java
+++ src/com/hard/learner/Calculator.java
@@ -3,6 +3,20 @@ package com.hard.learner;
 import java.util.Scanner;
 
 public class Calculator {
+	static String[] getValues(Scanner scanner) {
+		String value = scanner.nextLine();
+		String[] splitedValues = value.split(" ");
+		return splitedValues;
+	}
+
+	static int convert(String string) {
+		return Integer.parseInt(string);
+	}
+
+	static void print(int result) {
+		// TODO 실행 결과를 출력
+		System.out.println("합계 : " + result);
+	}
 
 	static int calculate(String symbol, int first, int second) {
 		// TODO 사칙 연산을 계산해 결과 값을 반환
@@ -23,22 +37,28 @@ public class Calculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		int result = Input.getFirstValue(scanner);
-
-		while (true) {
-			String symbol = Input.getSymbol(scanner);
-
-			if ("quit".equals(symbol)) {
-				Output.print(result);
-				break;
+		int result = 0;
+		String symbol = null;
+		
+		System.out.println("수식을 입력해 주세요.");
+		String[] splitedValues = getValues(scanner);
+		
+		int firstValue = convert(splitedValues[0]);
+		System.out.println("first : " + firstValue);
+		
+		for (int i = 1; i < splitedValues.length; i++) {
+			if (i % 2 == 1) {
+				symbol = splitedValues[i];
+				System.out.println("symbol : " + symbol);
+			} else {
+				int secondValue = convert(splitedValues[i]);
+				System.out.println("second : " + secondValue);
+				result = calculate(symbol, firstValue, secondValue);
+				firstValue = result;
 			}
-
-			int second = Input.getSecondValue(scanner);
-
-			result = calculate(symbol, result, second);
-
-			Output.print(result);
 		}
+
+		print(result);
 
 		scanner.close();
 	}
