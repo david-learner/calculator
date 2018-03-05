0998c7dada13f9863ba829ee51a18811cb58859d
David <xodnjs2549@naver.com>
2018-03-05 16:41:47 +0900

    두 개의 값을 입력받아 사칙연산

--- src/com/hard/learner/Calculator.java
+++ src/com/hard/learner/Calculator.java
@@ -1,5 +1,22 @@
 package com.hard.learner;
 
+import java.util.Scanner;
+
 public class Calculator {
-	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int first = 0;
+		int second = 0;
+		
+		System.out.println("연산에 필요한 첫 번째 숫자를 입력해주세요.");
+		first = scanner.nextInt();
+		System.out.println("연산에 필요한 두 번째 숫자를 입력해주세요.");
+		second = scanner.nextInt();
+		
+		System.out.println("덧셈 : " + (first + second));
+		System.out.println("뺄셈 : " + (first - second));
+		System.out.println("곱셈 : " + (first * second));
+		System.out.println("나눗셈 : " + (first / second));
+	}
 }
